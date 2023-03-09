29798696a9a8e0c21d1f9c9682cff28284cea9c3
Eileen Yoon <eyn@gmx.com>
2023-03-10 00:46:46 +0900

    compute: bump to buf module
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- compute/atan2.c
+++ compute/atan2.c
@@ -2,10 +2,11 @@
 /* Copyright 2022 Eileen Yoon <eyn@gmx.com> */
 
 #include "ane.h"
-#include "ane_arr.h"
+#include "ane_buf.h"
 #include "anec_atan2.h"
 
 #include <time.h>
+
 
 int main(void)
 {
@@ -16,36 +17,37 @@ int main(void)
 		return -1;
 	}
 
-	struct ane_arr *arr_a = ane_arr_init(nn, 0);
-	struct ane_arr *arr_b = ane_arr_init(nn, 1);
+	struct ane_buf *buf1 = ane_buf_init_input(nn, 0);
+	struct ane_buf *buf2 = ane_buf_init_input(nn, 1);
+	struct ane_buf *buf3 = ane_buf_init_output(nn, 0);
 
-	ane_fread(arr_a->data, arr_data_size(arr_a), "A.arr");
-	ane_fread(arr_b->data, arr_data_size(arr_b), "B.arr");
+	ane_fread(buf1->data, buf1->data_size, "A.arr");
+	ane_fread(buf2->data, buf2->data_size, "B.arr");
 
-	ane_arr_tile(arr_a);
-	ane_arr_tile(arr_b);
+	ane_buf_tile(buf1);
+	ane_buf_tile(buf2);
 
-	void *output = ane_zmemalign(output_size(nn, 0));
-
-
+#if 1
 	clock_t tic = clock();
 
-	ane_send(nn, arr_a->tile, 0);
-	ane_send(nn, arr_b->tile, 1);
+	ane_buf_send(nn, buf1);
+	ane_buf_send(nn, buf2);
 
 	err = ane_exec(nn);
 
-	ane_read(nn, output, 0);
+	ane_buf_read(nn, buf3);
 
 	clock_t toc = clock();
+
 	printf("Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
 
+	ane_buf_untile(buf3);
+	ane_fwrite(buf3->data, buf3->data_size, "atan2.arr");
+#endif
 
-	ane_fwrite(output, output_size(nn, 0), "atan2.tile");
-	free(output);
-
-	ane_arr_free(arr_a);
-	ane_arr_free(arr_b);
+	ane_buf_free(buf3);
+	ane_buf_free(buf2);
+	ane_buf_free(buf1);
 
 	ane_free(nn);
 
--- compute/sqrt.c
+++ compute/sqrt.c
@@ -2,42 +2,46 @@
 /* Copyright 2022 Eileen Yoon <eyn@gmx.com> */
 
 #include "ane.h"
-#include "ane_arr.h"
+#include "ane_buf.h"
 #include "anec_sqrt.h"
 
 #include <time.h>
+
 
 int main(void)
 {
 	int err = 0;
 	struct ane_nn *nn = ane_init_sqrt();
 	if (nn == NULL){
+		printf("error\n");
 		return -1;
 	}
 
-	struct ane_arr *arr = ane_arr_init(nn, 0);
-	ane_fread(arr->data, arr_data_size(arr), "A.arr");
-	ane_arr_tile(arr);
+	struct ane_buf *buf1 = ane_buf_init_input(nn, 0);
+	struct ane_buf *buf2 = ane_buf_init_output(nn, 0);
 
-	void *output = ane_zmemalign(output_size(nn, 0));
+	ane_fread(buf1->data, buf1->data_size, "A.arr");
+	ane_buf_tile(buf1);
 
-
+#if 1
 	clock_t tic = clock();
 
-	ane_send(nn, arr->tile, 0);
+	ane_buf_send(nn, buf1);
 
 	err = ane_exec(nn);
 
-	ane_read(nn, output, 0);
+	ane_buf_read(nn, buf2);
 
 	clock_t toc = clock();
+
 	printf("Elapsed: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
 
+	ane_buf_untile(buf2);
+	ane_fwrite(buf2->data, buf2->data_size, "sqrt.arr");
+#endif
 
-	ane_arr_free(arr);
-
-	ane_fwrite(output, output_size(nn, 0), "sqrt.tile");
-	free(output);
+	ane_buf_free(buf2);
+	ane_buf_free(buf1);
 
 	ane_free(nn);
 
