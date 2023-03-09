9ffe85f1a3bd05b475ba6db4698f5c3044b4ef12
Eileen Yoon <eyn@gmx.com>
2023-03-09 16:56:32 +0900

    compile: generate pyane.c
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,6 +1,9 @@
 
+> pyane.c
+echo "#include \"pyane.h\"" >> pyane.c
+
 for name in fcn srgan yolov5; do
 	anec.py -s -d hwx/$name.hwx
 	ld -r -b binary -o $name.anec.o $name.anec
-	echo "#include \"pyane_$name.h\""
+	echo "#include \"anec_$name.h\"" >> pyane.c
 done
